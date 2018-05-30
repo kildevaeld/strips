567192301347482b7b7ac7a0a756e8fbdd576434
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-05-31 00:35:25 +0200

    make app a bit smarter

--- strips/main.cpp
+++ strips/main.cpp
@@ -7,6 +7,8 @@
 #include <strips/os/os.h>
 #include <strips/path/path.h>
 #include <strips/prompt/prompt.h>
+#include <syrup/fs.h>
+#include <syrup/path.h>
 
 using namespace strips;
 namespace tem {
@@ -82,9 +84,44 @@ int main(int argc, char *argv[]) {
     return 3;
   }*/
 
-  
+  // std::string path(argv[1]);
+
+  char path[PATH_MAX];
+
+  if (!sy_file_exists(argv[1])) {
+    size_t idx, len;
+    len = sy_path_ext(argv[1], &idx);
+    if (len != 0) {
+      std::cerr << "file does not exists: " << argv[1] << std::endl;
+      return 5;
+    }
+    len = strlen(argv[1]);
+
+    memcpy(path, argv[1], len);
+    memcpy(path + len, ".js", 3);
+    path[len + 3] = '\0';
+
+    if (!sy_file_exists(path)) {
+      std::cerr << "file does not exists: " << path << std::endl;
+      return 5;
+    }
+  } else {
+    if (sy_file_is_regfile(argv[1])) {
+      strcpy(path, argv[1]);
+    } else if (sy_file_is_dir(argv[1])) {
+
+      if (!sy_path_join(path, argv[1], "index.js", NULL)) {
+        std::cerr << "could not resolve path" << std::endl;
+      }
+      if (!sy_file_exists(path)) {
+        std::cerr << "file does not exists: " << path << std::endl;
+        return 5;
+      }
+    }
+  }
+
   try {
-    auto result = vm.eval_path(argv[1]);
+    auto result = vm.eval_path(path);
   } catch (const std::runtime_error &e) {
     std::cerr << "could execute javascript: " << e.what() << std::endl;
     return 4;
